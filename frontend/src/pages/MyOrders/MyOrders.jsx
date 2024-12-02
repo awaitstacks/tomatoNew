b5cefd8b23265c8a245a2a33749f42c744f35410
awaitstacks <gvchannelid@gmail.com>
2024-12-02 10:04:24 +0530

    4

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import "./MyOrders.css";
 import axios from "axios";
-import { StoreContext } from "../../Context/StoreContext.jsx";
+import { StoreContext } from "../../context/StoreContext.jsx";
 import { assets } from "../../assets/assets";
 
 const MyOrders = () => {
